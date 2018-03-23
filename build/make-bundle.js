ff281f4f44cb949bed36b6b74d4ef10c7489b6de
Paleo <contact@paleo.casa>
2018-03-23 07:23:30 +0100

    New file structure

--- build/make-bundle.js
+++ build/make-bundle.js
@@ -8,7 +8,7 @@ const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
 const packageName = "mycn"
-const srcPath = path.join(__dirname, "src")
+const srcPath = path.join(__dirname, "..", "src")
 const compiledPath = path.join(__dirname, "compiled")
 const distNpmPath = path.join(__dirname, "..")
 
