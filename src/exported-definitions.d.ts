5d97fdf009ecd4659f69f4268b701fcf66041e5d
Paleo <contact@paleo.casa>
2018-09-29 11:55:43 +0100

    Overload connection methods instead to create additionnal methods

--- src/exported-definitions.d.ts
+++ src/exported-definitions.d.ts
@@ -1,24 +1,27 @@
-import { DatabaseConnection, ResultRow, PreparedStatement, ExecResult, TransactionConnection, QueryRunner } from "mycn"
-
-export interface QueryRunnerWithSqlBricks extends QueryRunner {
-  prepareSqlBricks<ROW extends ResultRow = any>(sqlBricks): Promise<PreparedStatement<ROW>>
-  execSqlBricks(sqlBricks): Promise<ExecResult>
-  allSqlBricks<ROW extends ResultRow = any>(sqlBricks): Promise<ROW[]>
-  singleRowSqlBricks<ROW extends ResultRow = any>(sqlBricks): Promise<ROW | undefined>
-  singleValueSqlBricks<VAL = any>(sqlBricks): Promise<VAL | undefined | null>
-}
-
-export interface DatabaseConnectionWithSqlBricks extends DatabaseConnection, QueryRunnerWithSqlBricks {
-  beginTransaction(): Promise<TransactionConnectionWithSqlBricks>
-}
-
-export interface TransactionConnectionWithSqlBricks extends TransactionConnection, QueryRunnerWithSqlBricks {
-}
+import { DatabaseConnection, ExecResult, PreparedStatement, QueryRunner, ResultRow, TransactionConnection, Cursor } from "mycn"
+import { Statement as SqlBricksQuery, SelectStatement as SqlBricksSelect } from "sql-bricks"
 
 export interface WithSqlBricksOptions {
   toParamsOptions?: {
     [key: string]: any
     placeholder?: string
   }
-  trace?(action: string, sqlBricks: any): void
+  trace?(action: string, sqlBricks: SqlBricksQuery): void
 }
+
+export type QueryRunnerWithSqlBricks = QueryRunner & {
+  prepare<R extends ResultRow = ResultRow>(sqlBricks: SqlBricksQuery): Promise<PreparedStatement<R>>
+  exec(sqlBricks: SqlBricksQuery): Promise<ExecResult>
+  all<R extends ResultRow = ResultRow>(sqlBricks: SqlBricksSelect): Promise<R[]>
+  singleRow<R extends ResultRow = ResultRow>(sqlBricks: SqlBricksSelect): Promise<R | undefined>
+  singleValue<V = unknown>(sqlBricks: SqlBricksSelect): Promise<V | undefined | null>
+  cursor<R extends ResultRow = ResultRow>(sqlBricks: SqlBricksSelect): Promise<Cursor<R>>
+}
+
+interface DatabaseConnectionWithSqlBricksTx extends DatabaseConnection {
+  // This method replaces the parent one
+  beginTransaction(): Promise<TransactionConnectionWithSqlBricks>
+}
+
+export type DatabaseConnectionWithSqlBricks = DatabaseConnectionWithSqlBricksTx & QueryRunnerWithSqlBricks
+export type TransactionConnectionWithSqlBricks = TransactionConnection & QueryRunnerWithSqlBricks
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,72 @@
-import { MycnOptions, createDatabaseConnection } from "mycn"
+import { createDatabaseConnection, DatabaseConnection, MycnOptions } from "mycn"
+import { SelectStatement as SqlBricksSelect, Statement as SqlBricksQuery } from "sql-bricks"
 import { DatabaseConnectionWithSqlBricks, WithSqlBricksOptions } from "./exported-definitions"
 
 export function createDatabaseConnectionWithSqlBricks(mycnOptions: MycnOptions, sbOptions: WithSqlBricksOptions = {}): DatabaseConnectionWithSqlBricks {
   return createDatabaseConnection({
     ...mycnOptions,
-    modifyConnection: (cn: any) => { // Use `any` because: https://github.com/Microsoft/TypeScript/issues/13995
-      cn.prepareSqlBricks = sqlBricks => {
-        if (sbOptions.trace)
-          sbOptions.trace("prepare", sqlBricks)
-        let params = sqlBricks.toParams(sbOptions.toParamsOptions)
-        return cn.prepare(params.text, params.values)
-      }
-      cn.execSqlBricks = sqlBricks => {
-        if (sbOptions.trace)
-          sbOptions.trace("exec", sqlBricks)
-        let params = sqlBricks.toParams(sbOptions.toParamsOptions)
-        return cn.exec(params.text, params.values)
-      }
-      cn.allSqlBricks = sqlBricks => {
-        if (sbOptions.trace)
-          sbOptions.trace("all", sqlBricks)
-        let params = sqlBricks.toParams(sbOptions.toParamsOptions)
-        return cn.all(params.text, params.values)
-      }
-      cn.singleRowSqlBricks = sqlBricks => {
-        if (sbOptions.trace)
-          sbOptions.trace("singleRow", sqlBricks)
-        let params = sqlBricks.toParams(sbOptions.toParamsOptions)
-        return cn.singleRow(params.text, params.values)
-      }
-      cn.singleValueSqlBricks = sqlBricks => {
-        if (sbOptions.trace)
-          sbOptions.trace("singleValue", sqlBricks)
-        let params = sqlBricks.toParams(sbOptions.toParamsOptions)
-        return cn.singleValue(params.text, params.values)
-      }
-      if (mycnOptions.modifyConnection)
-        cn = mycnOptions.modifyConnection(cn)
-      return cn
-    }
+    modifyConnection: cn => modifyConnection(cn, mycnOptions, sbOptions),
   }) as DatabaseConnectionWithSqlBricks
+}
+
+function modifyConnection(parent: DatabaseConnection, mycnOptions: MycnOptions, sbOptions: WithSqlBricksOptions) {
+  let modified = Object.create(parent)
+
+  modified.prepare = (sql: string | SqlBricksQuery, params?) => {
+    if (forParent(sql))
+      return parent.prepare(sql, params)
+    if (sbOptions.trace)
+      sbOptions.trace("prepare", sql)
+    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+    return parent.prepare(text, values)
+  }
+  modified.exec = (sql: string | SqlBricksQuery, params?) => {
+    if (forParent(sql))
+      return parent.exec(sql, params)
+    if (sbOptions.trace)
+      sbOptions.trace("exec", sql)
+    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+    return parent.exec(text, values)
+  }
+  modified.all = (sql: string | SqlBricksSelect, params?) => {
+    if (forParent(sql))
+      return parent.all(sql, params)
+    if (sbOptions.trace)
+      sbOptions.trace("all", sql)
+    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+    return parent.all(text, values)
+  }
+  modified.singleRow = (sql: string | SqlBricksSelect, params?) => {
+    if (forParent(sql))
+      return parent.singleRow(sql, params)
+    if (sbOptions.trace)
+      sbOptions.trace("singleRow", sql)
+    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+    return parent.singleRow(text, values)
+  }
+  modified.singleValue = (sql: string | SqlBricksSelect, params?) => {
+    if (forParent(sql))
+      return parent.singleValue(sql, params)
+    if (sbOptions.trace)
+      sbOptions.trace("singleValue", sql)
+    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+    return parent.singleValue(text, values)
+  }
+  modified.cursor = (sql: string | SqlBricksSelect, params?) => {
+    if (forParent(sql))
+      return parent.cursor(sql, params)
+    if (sbOptions.trace)
+      sbOptions.trace("cursor", sql)
+    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+    return parent.cursor(text, values)
+  }
+
+  if (mycnOptions.modifyConnection)
+    modified = mycnOptions.modifyConnection(modified)
+
+  return modified
+}
+
+function forParent(sql: string | SqlBricksQuery): sql is string {
+  return typeof sql === "string"
 }
