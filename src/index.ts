5c76e74c834932962e7a529e70592744b6ece090
Paleo <contact@paleo.casa>
2018-09-30 15:06:22 +0100

    Upgrade LADC

--- src/index.ts
+++ src/index.ts
@@ -9,64 +9,24 @@ export function createDatabaseConnectionWithSqlBricks(ladcOptions: LadcOptions, 
   }) as DatabaseConnectionWithSqlBricks
 }
 
+const methodNames = ["prepare", "exec", "all", "singleRow", "singleValue", "cursor"]
+
 function modifyConnection(parent: DatabaseConnection, ladcOptions: LadcOptions, sbOptions: WithSqlBricksOptions) {
   let modified = Object.create(parent)
 
-  modified.prepare = (sql: string | SqlBricksQuery, params?) => {
-    if (forParent(sql))
-      return parent.prepare(sql, params)
-    if (sbOptions.trace)
-      sbOptions.trace("prepare", sql)
-    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
-    return parent.prepare(text, values)
-  }
-  modified.exec = (sql: string | SqlBricksQuery, params?) => {
-    if (forParent(sql))
-      return parent.exec(sql, params)
-    if (sbOptions.trace)
-      sbOptions.trace("exec", sql)
-    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
-    return parent.exec(text, values)
-  }
-  modified.all = (sql: string | SqlBricksSelect, params?) => {
-    if (forParent(sql))
-      return parent.all(sql, params)
-    if (sbOptions.trace)
-      sbOptions.trace("all", sql)
-    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
-    return parent.all(text, values)
-  }
-  modified.singleRow = (sql: string | SqlBricksSelect, params?) => {
-    if (forParent(sql))
-      return parent.singleRow(sql, params)
-    if (sbOptions.trace)
-      sbOptions.trace("singleRow", sql)
-    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
-    return parent.singleRow(text, values)
-  }
-  modified.singleValue = (sql: string | SqlBricksSelect, params?) => {
-    if (forParent(sql))
-      return parent.singleValue(sql, params)
-    if (sbOptions.trace)
-      sbOptions.trace("singleValue", sql)
-    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
-    return parent.singleValue(text, values)
-  }
-  modified.cursor = (sql: string | SqlBricksSelect, params?) => {
-    if (forParent(sql))
-      return parent.cursor(sql, params)
-    if (sbOptions.trace)
-      sbOptions.trace("cursor", sql)
-    let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
-    return parent.cursor(text, values)
+  for (let method of methodNames) {
+    modified[method] = (sql: string | SqlBricksQuery, params?) => {
+      if (typeof sql === "string")
+        return parent[method](sql, params)
+      if (sbOptions.trace)
+        sbOptions.trace(method, sql)
+      let { text, values } = sql.toParams(sbOptions.toParamsOptions as any)
+      return parent[method](text, values)
+    }
   }
 
   if (ladcOptions.modifyConnection)
     modified = ladcOptions.modifyConnection(modified)
 
   return modified
-}
-
-function forParent(sql: string | SqlBricksQuery): sql is string {
-  return typeof sql === "string"
 }
