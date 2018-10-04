e61b47ca3f515774c8e520cb03117b49f56ebc2d
Paleo <contact@paleo.casa>
2018-10-04 09:34:11 +0100

    Upgrade ladc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { BasicDatabaseConnection } from "ladc"
+import { BasicDatabaseConnection, LadcAdapter } from "ladc"
 import { ClientConfig } from "pg"
 import { createPgConnection, toBasicDatabaseConnection } from "./BasicDatabaseConnection"
 import { LadcPgOptions } from "./exported-definitions"
 
-export function pgConnectionProvider(config: string | ClientConfig, ladcPgOptions: LadcPgOptions = {}): () => Promise<BasicDatabaseConnection> {
-  return async () => {
-    let db = await createPgConnection(config)
-    return toBasicDatabaseConnection(db, ladcPgOptions)
+export default function pgAdapter(config: string | ClientConfig, ladcPgOptions: LadcPgOptions = {}): LadcAdapter {
+  return {
+    createConnection: async () => {
+      let db = await createPgConnection(config)
+      return toBasicDatabaseConnection(db, ladcPgOptions)
+    }
   }
 }
