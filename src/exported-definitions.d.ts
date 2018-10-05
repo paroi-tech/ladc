c6670b1e0edc37a3de44f71406aac491d5fdaae3
Paleo <contact@paleo.casa>
2018-10-05 16:56:37 +0100

    Put the 'pg' configuration in options

--- src/exported-definitions.d.ts
+++ src/exported-definitions.d.ts
@@ -1,4 +1,7 @@
+import { ClientConfig } from "pg"
+
 export interface LadcPgOptions {
+  pg: string | ClientConfig,
   getAutoincrementedIdColumnName?(tableName: string): string | undefined
   useReturningAll?: boolean
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { LadcAdapter } from "ladc"
-import { ClientConfig } from "pg"
 import { createPgConnection, toBasicDatabaseConnection } from "./BasicDatabaseConnection"
 import { LadcPgOptions } from "./exported-definitions"
 
-export default function pgAdapter(config: string | ClientConfig, ladcPgOptions: LadcPgOptions = {}): LadcAdapter {
+export default function pgAdapter(options: LadcPgOptions): LadcAdapter {
   return {
     createConnection: async () => {
-      let db = await createPgConnection(config)
-      return toBasicDatabaseConnection(db, ladcPgOptions)
+      let db = await createPgConnection(options.pg)
+      return toBasicDatabaseConnection(db, options)
     }
   }
 }
